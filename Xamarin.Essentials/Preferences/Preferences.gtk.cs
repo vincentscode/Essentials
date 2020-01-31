8aa8249f7ae2385566550095fd7253be5828fe64
Vincent Schmandt <vincentscode@gmail.com>
2020-02-01 00:32:40 +0100

    removed forced style

--- Xamarin.Essentials/Preferences/Preferences.gtk.cs
+++ Xamarin.Essentials/Preferences/Preferences.gtk.cs
@@ -1,20 +1,22 @@
 ﻿namespace Xamarin.Essentials
 {
+    // TODO
     public static partial class Preferences
     {
-        static bool PlatformContainsKey(string key, string sharedName) =>
-            throw ExceptionUtils.NotSupportedOrImplementedException;
+        static bool PlatformContainsKey(string key, string sharedName) => false;
 
-        static void PlatformRemove(string key, string sharedName) =>
-            throw ExceptionUtils.NotSupportedOrImplementedException;
+        static void PlatformRemove(string key, string sharedName) {
 
-        static void PlatformClear(string sharedName) =>
-            throw ExceptionUtils.NotSupportedOrImplementedException;
+        }
 
-        static void PlatformSet<T>(string key, T value, string sharedName) =>
-            throw ExceptionUtils.NotSupportedOrImplementedException;
+        static void PlatformClear(string sharedName) {
 
-        static T PlatformGet<T>(string key, T defaultValue, string sharedName) =>
-            throw ExceptionUtils.NotSupportedOrImplementedException;
+        }
+
+        static void PlatformSet<T>(string key, T value, string sharedName) {
+        
+        }
+
+        static T PlatformGet<T>(string key, T defaultValue, string sharedName) => defaultValue;
     }
 }
